b8ef2097e9a077d5e217e727bb39f1b27a2c4b4f
Zibbp <zibbp@zibbp.net>
2023-12-27 10:47:56 -0600

    fix(entrypoint): start server

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 if [ "$(uname -m)" = "x86_64" ]; then
-  exec /usr/local/bin/temporal-amd64 "$@"
+  exec /usr/local/bin/temporal-amd64 server start-dev --db-filename /data/temporal.db --ip 0.0.0.0
 elif [ "$(uname -m)" = "aarch64" ]; then
-  exec /usr/local/bin/temporal-arm64 "$@"
+  exec /usr/local/bin/temporal-arm64 server start-dev --db-filename /data/temporal.db --ip 0.0.0.0
 else
   echo "Unsupported architecture: $(uname -m)"
   exit 1
