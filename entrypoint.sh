823f1993f69e0b6c5a01f58eaa1e151dc061dc7f
Zibbp <zibbp@zibbp.net>
2024-01-03 17:18:19 -0600

    fix(entrypoint): drop retention to 30 days

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
+
 if [ "$(uname -m)" = "x86_64" ]; then
+  sleep 5 && /usr/local/bin/temporal-amd64 operator namespace update --retention 30 default && /usr/local/bin/temporal-amd64 operator namespace list &
   exec /usr/local/bin/temporal-amd64 server start-dev --db-filename /data/temporal.db --ip 0.0.0.0
 elif [ "$(uname -m)" = "aarch64" ]; then
+  sleep 5 && /usr/local/bin/temporal-arm64 operator namespace update --retention 30 default && /usr/local/bin/temporal-arm64 operator namespace list &
   exec /usr/local/bin/temporal-arm64 server start-dev --db-filename /data/temporal.db --ip 0.0.0.0
 else
   echo "Unsupported architecture: $(uname -m)"
   exit 1
 fi
+
