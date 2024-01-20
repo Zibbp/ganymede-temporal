cfb9c9013cd11703dc180ac52ac8913d63d3ea77
Zibbp <zibbp@zibbp.net>
2024-01-20 10:23:44 -0600

    fix(entrypoint): memory only mode

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,18 @@
 
 if [ "$(uname -m)" = "x86_64" ]; then
   sleep 5 && /usr/local/bin/temporal-amd64 operator namespace update --retention 30 default && /usr/local/bin/temporal-amd64 operator namespace list &
-  exec /usr/local/bin/temporal-amd64 server start-dev --db-filename /data/temporal.db --ip 0.0.0.0 --namespace default --namespace temporal-system
+  if [ -z "$MEMORY_ONLY" ]; then
+    exec /usr/local/bin/temporal-amd64 server start-dev --db-filename /data/temporal.db --ip 0.0.0.0 --namespace default --namespace temporal-system
+  else
+    exec /usr/local/bin/temporal-amd64 server start-dev --ip 0.0.0.0 --namespace default --namespace temporal-system
+  fi 
 elif [ "$(uname -m)" = "aarch64" ]; then
   sleep 5 && /usr/local/bin/temporal-arm64 operator namespace update --retention 30 default && /usr/local/bin/temporal-arm64 operator namespace list &
-  exec /usr/local/bin/temporal-arm64 server start-dev --db-filename /data/temporal.db --ip 0.0.0.0 --namespace default --namespace temporal-system
+  if [ -z "$MEMORY_ONLY" ]; then
+    exec /usr/local/bin/temporal-arm64 server start-dev --db-filename /data/temporal.db --ip 0.0.0.0 --namespace default --namespace temporal-system
+  else
+    exec /usr/local/bin/temporal-arm64 server start-dev --ip 0.0.0.0 --namespace default --namespace temporal-system
+  fi 
 else
   echo "Unsupported architecture: $(uname -m)"
   exit 1
